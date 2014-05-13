6e0e4f0bb86d2c320635e0614fd449ac0aa92ef5
XeryusTC <armadillo@onenetbeyond.org>
2014-05-13 22:27:52 +0200

    Make the buy drink page functional

--- pointofsale/forms.py
+++ pointofsale/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pointofsale.models import Drink, Account
+from pointofsale.models import Drink, Account, DrinkOrder
 #from django.forms.extras.widgets import RadioSelect
 
 class BuyDrinkForm(forms.Form):
@@ -11,3 +11,20 @@ class BuyDrinkForm(forms.Form):
         
         self.fields['drink'].choices = ( (drink.pk, drink.name) for drink in Drink.objects.all() )
         self.fields['account'].choices = ( (account.pk, account.pk) for account in Account.objects.order_by('user__first_name') if account.get_credits_left() > 0)
+    
+    def clean(self):
+        super(BuyDrinkForm, self).clean()
+        
+        # Check if the account can afford the drink that it wants to buy, raise an error if this is not the case
+        account = Account.objects.get(pk = self.cleaned_data['account'])
+        drink = Drink.objects.get(pk = self.cleaned_data['drink'])
+        
+        if drink.price > account.get_credits_left():
+            raise forms.ValidationError("%(name)s has insufficient funds to buy %(drink)s" % {'name': account.user.get_full_name(), 'drink': drink.name})
+        return self.cleaned_data
+    
+    def buy_drink(self):
+        account = Account.objects.get(pk = self.cleaned_data['account'])
+        drink = Drink.objects.get(pk = self.cleaned_data['drink'])
+        DrinkOrder.objects.create(drink = drink, account = account)
+
--- pointofsale/views.py
+++ pointofsale/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import edit
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect
 
 from pointofsale.forms import BuyDrinkForm
 
-from pointofsale.models import Drink, Account
+from pointofsale.models import Drink, Account, DrinkOrder
 
 # Create your views here.
 class BuyDrinkView(edit.FormView):
     template_name = 'pointofsale/buydrink.html'
     form_class = BuyDrinkForm
+    success_url = reverse_lazy('pos:buy_drink')
+    
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(BuyDrinkView, self).dispatch(*args, **kwargs)
     
     def get_context_data(self, **kwargs):
         context = super(BuyDrinkView, self).get_context_data(**kwargs)
@@ -21,6 +30,13 @@ class BuyDrinkView(edit.FormView):
         context['accounts'] = {}
         for account in Account.objects.all():
             if account.get_credits_left() > 0:
-                context['accounts'][account.pk] = {'credits': account.credits, 'used': account.get_credits_used(), 'left': account.get_credits_left(), 'name': account.user.get_full_name() }
+                context['accounts'][account.pk] = {'credits': account.credits, 'used': account.get_credits_used(),
+                        'left': account.get_credits_left(), 'name': account.user.get_full_name() }
         
         return context
+
+    #@method_decorator(login_required)
+    #@method_decorator(require_POST)
+    def form_valid(self, form):
+        form.buy_drink()
+        return super(BuyDrinkView, self).form_valid(form)
